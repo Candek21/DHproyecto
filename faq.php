ce225412f6cfc0fd5edd35b9e105be3783539f51
candek21 <cangrosso@gmail.com>
2019-12-18 12:53:50 -0300

    Ahora si - correción de commit

--- faq.php
+++ faq.php
@@ -11,10 +11,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Bienvenidos a Vulture - F:A:Q</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <script src="https://kit.fontawesome.com/0fbb5c7ed7.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="css/home - style.css">
-    <title>FAQs de Vulture</title>
 </head>
 
 <body>
