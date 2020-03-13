5e217a88afb697bb63cb3fa3f4343760688f09df
Javier <javiermacedo@infosir.com.ar>
2020-03-13 16:42:41 -0300

    temporalmente puse una base del servidor de internet aunq puee quedar

--- db.php
+++ db.php
@@ -3,7 +3,8 @@
 
         // $dsn = 'mysql:host=127.0.0.1;dbname=buitre_db;port3306';
         $dsn = 'mysql:host=190.210.222.204;dbname=buitre_db;port3306';
-        $db_usr ='buitre';
+        $dsn = 'mysql:host=infosir.com.ar;dbname=infosir_buitredb;port3306';
+        $db_usr ='infosir_buitre';
         $db_pass = 'cancrinachja';
 
         try{
