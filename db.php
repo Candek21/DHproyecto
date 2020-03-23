a1b669ee45a0d1bea0e6768120d9f814399e352f
Javier <javiermacedo@infosir.com.ar>
2020-03-23 14:47:31 -0300

    movi a poner la de internet pq se tildo la maquina de la ofi jajaja

--- db.php
+++ db.php
@@ -3,8 +3,12 @@
 
         // $dsn = 'mysql:host=127.0.0.1;dbname=buitre_db;port3306';
         // si no funciona con infosir.no-ip.org usar 190.195.133.61
-        $dsn = 'mysql:host=infosir.no-ip.org;dbname=buitre_db;port3306';
-        $db_usr ='buitre';
+        //$dsn = 'mysql:host=infosir.no-ip.org;dbname=buitre_db;port3306';
+        //$db_usr ='buitre';
+
+        $dsn = 'mysql:host=infosir.com.ar;dbname=infosir_buitredb;port3306';
+        $db_usr ='infosir_buitre';
+
         $db_pass = 'cancrinachja';
 
         try{
